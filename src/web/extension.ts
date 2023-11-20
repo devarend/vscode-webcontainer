7fc0f9dcb9ab13764f08757011860ef3db1a46f4
devarend <devarend@gmail.com>
2023-11-20 22:57:18 +0100

    create webview panel when you run the command

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,23 +1,21 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {window, ViewColumn, ExtensionContext, commands} from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+export function activate(context: ExtensionContext) {
 	console.log('Congratulations, your extension "vscode-webcontainer" is now active in the web extension host!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-webcontainer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-webcontainer in a web extension host!');
+	let disposable = commands.registerCommand('vscode-webcontainer.helloWorld', () => {
+		window.showInformationMessage('Hello World from vscode-webcontainer in a web extension host!');
+		const panel = window.createWebviewPanel(
+			'serverPanel',
+			'terminal',
+			ViewColumn.One,
+			{
+				enableScripts: true,
+				retainContextWhenHidden: true,
+				localResourceRoots: []
+			}
+		)
 	});
 
 	context.subscriptions.push(disposable);
